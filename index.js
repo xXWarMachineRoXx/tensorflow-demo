6ffb0167d0eab624f1d01b5730de27fafd3212d9
xxXWarMachineRoXxx <rijul123.sharma@outlook.com>
2023-01-11 20:59:30 +0530

    fixed index.js to use node.js as server to flask and added some post data code that would be modified from lines 40 to 55 in index.js

--- index.js
+++ index.js
@@ -27,14 +27,37 @@ app.listen(3000, function () {
     
 // });
 app.get('/flask', function(req, res) {
-  request('http://127.0.0.1:8001/flask/', function (error, response, body) {
+  request('http://127.0.0.1:8001/flask', function (error, response, body) {
       console.error('error:', error); // Print the error
       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
       console.log('body:', body); // Print the data received
       res.send(body); //Display the response on the website
     });      
 });
-app.get('/home', function(req, res) {
+
+async function postData(url = '', data = {}) {
+  // Default options are marked with *
+  const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: 'same-origin', // include, *same-origin, omit
+    headers: {
+      'Content-Type': 'application/json'
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    redirect: 'follow', // manual, *follow, error
+    referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    body: JSON.stringify(data) // body data type must match "Content-Type" header
+  });
+  return response.json(); // parses JSON response into native JavaScript objects
+}
+
+postData('https://example.com/answer', { answer: 42 })
+  .then((data) => {
+    console.log(data); // JSON data parsed by `data.json()` call
+  });
+app.get('/cat', function(req, res) {
   request('http://127.0.0.1:8001/flask/cat', function (error, response, body) {
       console.error('error:', error); // Print the error
       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
